847401b19ce7df7ea4d03847ad10195487f19004
Donald King <chronos@chronos-tachyon.net>
2015-05-31 13:28:07 -0700

    Add test for unpacking deep, manually-nested MultipleErrors.

--- multierror_test.go
+++ multierror_test.go
@@ -17,28 +17,42 @@ var (
 )
 
 func ExampleMultipleErrors_empty() {
-	fmt.Printf("%T: %[1]v", Of())
+	fmt.Printf("%T: %[1]v\n", Of())
 	// Output:
 	// <nil>: <nil>
 }
 
 func ExampleMultipleErrors_nil() {
-	fmt.Printf("%T: %[1]v", Of(nil, nil, nil))
+	fmt.Printf("%T: %[1]v\n", Of(nil, nil, nil))
 	// Output:
 	// <nil>: <nil>
 }
 
 func ExampleMultipleErrors_one() {
-	fmt.Printf("%T: %[1]v", Of(errorA))
+	fmt.Printf("%T: %[1]v\n", Of(errorA))
 	// Output:
 	// multierror.exampleError: A
 }
 
 func ExampleMultipleErrors_many() {
-	fmt.Printf("%T: %[1]v", Of(errorA, errorB, errorC))
+	fmt.Printf("%T: %[1]v\n", Of(errorA, errorB, errorC))
 	// Output:
 	// multierror.MultipleErrors: encountered multiple errors:
 	//	... A
 	//	... B
 	//	... C
+}
+
+func ExampleMultipleErrors_nested() {
+	inner := MultipleErrors{errorA, nil}
+	mid := MultipleErrors{inner, nil, errorB}
+	outer := Of(mid, errorC)
+	fmt.Printf("%T: %[1]v\n", Of(errorA, errorB, errorC))
+	fmt.Printf("Len=%d\n", len(outer.(MultipleErrors)))
+	// Output:
+	// multierror.MultipleErrors: encountered multiple errors:
+	//	... A
+	//	... B
+	//	... C
+	// Len=3
 }
